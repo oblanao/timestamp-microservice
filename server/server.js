10ab29013199df6a0dd159263f4bf51382bac486
Bogdan Posedaru <bogdan.posedaru@gmail.com>
2018-04-03 12:14:22 +0300

    Initial config of express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const publicPath = path.join(__dirname+'/../public');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('bau!').end();
+})
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+})
